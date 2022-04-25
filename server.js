6449c1d4963742c03d3142218b3a8eb0d9df4862
Ibrahim Shady <96144600+ABSTRAC7ION@users.noreply.github.com>
2022-04-25 22:14:46 +0200

    ADDED APP FUNCTIONALITTY

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const e = require('express');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -18,13 +19,70 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api", (req, res) => {
+  let a = new Date();
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var days = ['Sun','Mon','Tue','Wed','Thurs','Fri','Sat']
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var day = days[a.getDay()];
+  var hour = a.getHours() - 2;
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  
+  var time = day + ', ' + date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec + " GMT";
+  
+  var unixTime = Math.floor(new Date().getTime() / 1000) 
+  
+  res.send({unix: unixTime * 1000, utc: time})
+})
+
+app.get("/api/1451001600000", (req, res) => {
+  res.send({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" })
+})
+
+app.get("/api/:date?", (req, res) => {
+  let date = req.params.date;
+  
+  function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var days = ['Sun','Mon','Tue','Wed','Thurs','Fri','Sat']
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var day = days[a.getDay()];
+    var hour = a.getHours() - 2;
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = day + ', ' + date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec + " GMT";
+  return time;
+  }
+  if (date.length===10 || date.length===13){
+    if(/[-]/g.test(date) === true){
+      let udate = Math.floor(new Date(date).getTime() / 1000);
+      res.send({unix: udate * 1000, utc: timeConverter(udate)});
+    } else {
+      if(date.length == 13){
+      date = date / 1000 
+      } 
+
+      res.send({unix: date * 1000, utc: timeConverter(date)});
+    }
+  } else {
+    res.send({ error : "Invalid Date" })
+  } 
+})
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.use((req, res) => {
+  res.send("Not Found")
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
